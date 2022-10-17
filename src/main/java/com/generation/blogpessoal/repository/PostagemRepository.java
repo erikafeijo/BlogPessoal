ea2a47b7ab9eefd677de7451c1a8039e8a13f7f4
lucasbrlos <feijoerika@gmail.com>
2022-10-17 01:47:22 -0300

    atualizacao projeto

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -2,16 +2,16 @@ package com.generation.blogpessoal.repository;
 
 import java.util.List;
 
-import com.generation.blogpessoal.model.Postagem;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.generation.blogpessoal.model.Postagem;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
-    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
-   
+    
+    //É uma regra específica para o negócio, não é um padrão como no controller, então precisa criar primeiro aqui para usar lá
+    public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo); //Usa param para encontrar a partir de uma parte do título, começou a digitar, ele já encontra
 
 }
