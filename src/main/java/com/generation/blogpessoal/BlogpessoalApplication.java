7924ab75abc8e750d63ffb3218bb33b4242d6b57
Erika Feijo <100244232+erikafeijo@users.noreply.github.com>
2022-10-20 13:53:38 -0300

    Update BlogpessoalApplication.java

--- src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class blogpessoalApplication {
+public class BlogpessoalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(blogpessoalApplication.class, args);
